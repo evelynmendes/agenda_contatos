8caa754d50f8f523399bdad7a28968f274407f78
Evelyn Mendes <evelyn.mendes@gmail.com>
2020-10-10 20:21:04 -0300

    banco criado

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import 'io/contact_page.dart';
+import 'io/home_page.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    home: ContactPage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
